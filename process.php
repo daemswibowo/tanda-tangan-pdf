c12c9c7fe69078210b171829dec11dff80144fca
Dimas Wibowo <daemswibowo@gmail.com>
2018-08-23 06:35:54 +0800

    comment

--- process.php
+++ process.php
@@ -2,6 +2,7 @@
 use setasign\Fpdf;
 use setasign\Fpdi;
 require 'vendor/autoload.php';
+
 $x = $_POST['x'];
 $y = $_POST['y'];
 $pdf = new Fpdi\Fpdi();
@@ -23,13 +24,6 @@ for ($pageNo=1; $pageNo<=$pagecount; $pageNo++) {
 	}
 	$pdf->useTemplate($templateID, -10, 20, 210);
 }
-//Use this page as template
-// use the imported page and place it at point 20,30 with a width of 170 mm
-
-#Print Hello World at the bottom of the page
-
-//Select Arial italic 8
-// $pdf->Image('download.png',45,220,15,10);
 
 $pdf->Output("modified_pdf.pdf", "F");
 ?>
